684d88b57fb3dba3f76b4bfff509e8407b1493cf
iguissouma <issam.guissouma@laposte.net>
2019-11-15 18:42:19 +0100

    chore: update to arrow-kt 0.10.0 add maven as build add docker-compose for mongo

--- src/main/kotlin/org/smartjava/kotlin/itemservice/rest/ItemRoutes.kt
+++ src/main/kotlin/org/smartjava/kotlin/itemservice/rest/ItemRoutes.kt
@@ -1,7 +1,7 @@
 package org.smartjava.kotlin.itemservice.rest
 
-import arrow.effects.*
-import arrow.effects.monok.monad.binding
+import arrow.fx.reactor.*
+import arrow.fx.reactor.extensions.fx
 import org.smartjava.kotlin.itemservice.Config.BASE_ROUTE
 import org.smartjava.kotlin.itemservice.model.Item
 import org.smartjava.kotlin.itemservice.service.ItemService
@@ -36,14 +36,14 @@ object ItemsRoute {
         }
 
         fun getOrder(request: ServerRequest): Mono<ServerResponse> = okJson {
-            binding {
+            MonoK.fx {
                 val uuid = pathParamToUUID(request, PATH_ID).bind()
                 itemService.getItem(uuid).bind()
             }
         }
 
         fun createOrder(request: ServerRequest): Mono<ServerResponse> = okJson {
-            binding {
+            MonoK.fx {
                 val item = request.bodyToMono<Item>().k().bind()
                 itemService.createItem(item).bind()
             }
